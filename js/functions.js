fb1312a20f5d8e13fcee168a8ea6e43561c8c30e
MaksimDoronin <maksdoroninone@yandex.ru>
2025-03-31 17:53:32 +0300

    Добавляю первую функцию

--- js/functions.js
+++ js/functions.js
@@ -2,8 +2,10 @@ function checkingLengthString (lengthString, maxLengthString) {
   console.log(lengthString.length);
   if(lengthString.length <= maxLengthString) {
     console.log(true);
+    return true;
   }
   console.log(false);
+  return false;
 }
 
-checkingLengthString('Hello World!', 10);
+checkingLengthString('проверяемая строка', 10);
