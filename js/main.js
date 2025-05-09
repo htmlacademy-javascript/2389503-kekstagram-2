914e415c31fb609978dd5ef9c7e8ee96d26ee6c4
MaksimDoronin <maksdoroninone@yandex.ru>
2025-05-09 18:20:27 +0300

    Написал функцию, которая создаёт объекты с фотографиями

--- js/main.js
+++ js/main.js
@@ -1,51 +1,3 @@
-/*
-  Объект состоит из пяти ключей:
-
-  1. id -  идентификатор опубликованной фотографии. Это число от 1 до 25. Идентификаторы не должны повторяться.
-
-  2. url - строка — адрес картинки вида `photos/{{i}}.jpg`, где `{{i}}` — это число от 1 до 25. Адреса картинок не должны повторяться.
-
-  3. description - строка - описание фотографии. Придумываю самостоятельно.
-
-  4. likes - количество лайков, поставленных фотографии. Случайное число от 15 до 200.
-
-  5. comments - массив, состоящий из комментариев, оставленных другими пользователями, где каждым комментарием является объект. Количество комментариев к каждой фотографии - случайное число от 0 до 30. Каждый объект имеет 4 ключа:
-
-    1. id - любое неповторяющееся число
-
-    2. avatar - строка, значение которой формируется по правилу img/avatar-{{случайное число от 1 до 6}}.svg
-
-    3. message - строка, которую я получаю из конкретного массива(ниже), который содержит в себе строковые элементы.
-
-    const messages = [
-      'Всё отлично!',
-      'В целом всё неплохо. Но не всё.',
-      'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-      'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-      'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-      'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-    ]
-
-    4. name - случайная строка, которую я подбираю из массива, содержащего строки(имена). Создаю самостоятельно.
-
-    const names = [
-      "Алексей",
-      "Мария",
-      "Иван",
-      "Екатерина",
-      "Дмитрий",
-      "Ольга",
-      "Сергей",
-      "Наталья",
-      "Михаил",
-      "Анна"
-    ];
-
-    Таким образом получаю вот такую структуру:
-
-    [ { id: 0, avatar: 'img/avatar-0.svg', message: 'Всё отлично!', name: 'Иван' } ]
-*/
-
 const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -69,20 +21,41 @@ const NAMES = [
 ];
 
 const DESCRIPTIONS = [
-  'Солнце',
-  'Травка',
-  'Зелёная',
-  'Лягушка',
-  'Замочный',
+  'Пустой пляж с лежаками и зонтами у спокойного залива, окружённого зеленью и современными зданиями на заднем плане',
+  'Указатель на пляж',
+  'Живописная бухта',
+  'Девушка в купальнике с фотоаппаратом',
+  'Две миски с рисовым супом',
+  'Суперкар чёрного матового цвета с открытой дверью',
+  'Клубника, разрезанная пополам на деревянной тарелке',
+  'Два стакана с красным виноградным соком',
+  'Самолёт, пролетающий над головами купающихся людей на пляже',
+  'Выдвинутый снизу ящик с обувью',
+  'Песчаный пляж, разделённый секциями с садами',
+  'Спортивный автомобиль ауди белого цвета',
+  'Овощной салат с куском рыбы',
+  'Кот, привязанный к подушке, в виде суши',
+  'Часть интерьера гостиной с закинутыми ногами человека на подлокотник дивана',
+  'Высоко в небе над хребтами гор',
+  'Концерт',
+  'Американский маслкар в кирпичном помещении',
+  'Ночные тапки с фонариками',
+  'Пальмы на территории отеля в ночное время',
+  'Мясное блюдо с ломтиком лимона',
+  'Живописный заказ на берегу океана',
+  'Затаившийся краб на суше',
+  'Вид из зала на сцену во время концерта',
+  'Бегемот вынырнул перед машиной на переправе через реку',
 ];
 
 const MIN_COUNT = 1;
 const MAX_COUNT = 25;
-const MIN_COUNT_MESSAGE = 0;
-const MAX_COUNT_MESSAGE = 30;
+const MIN_COUNT_COMMENTS = 0;
+const MAX_COUNT_COMMENTS = 30;
 const MIN_COUNT_AVATAR = 1;
 const MAX_COUNT_AVATAR = 6;
 const MIN_COUNT_NAMES = 0;
+const MIN_COUNT_MESSAGES = 0;
 const MIN_COUNT_DESCRIPTION = 0;
 const MIN_LIKES = 15;
 const MAX_LIKES = 200;
@@ -110,16 +83,22 @@ const getRandomArrayElement = (min, max) => {
   };
 };
 
+const getRandomPhotoId = getRandomArrayElement(MIN_COUNT, MAX_COUNT);
+const getRandomUrlId = getRandomArrayElement(MIN_COUNT, MAX_COUNT);
+const getRandomNumbersOfLikes = getRandomArrayElement(MIN_LIKES, MAX_LIKES);
+const getRandomDescription = getRandomArrayElement(MIN_COUNT_DESCRIPTION, DESCRIPTIONS.length - 1);
+const getRandomNumbersOfComments = getRandomArrayElement(MIN_COUNT_COMMENTS, MAX_COUNT_COMMENTS);
+
 const getComments = () => {
-  const count = getRandomPositiveInteger(MIN_COUNT_MESSAGE, MAX_COUNT_MESSAGE);
+  const count = getRandomNumbersOfComments();
   const comments = [];
 
   for (let i = 1; i <= count; i++) {
     comments.push(
       {
-        id: getRandomPositiveInteger(MIN_COUNT, MAX_COUNT_MESSAGE),
+        id: getRandomPositiveInteger(MIN_COUNT , MAX_LIKES),
         avatar: `img/avatar-${getRandomPositiveInteger(MIN_COUNT_AVATAR, MAX_COUNT_AVATAR)}.svg`,
-        message: MESSAGES[getRandomPositiveInteger(MIN_COUNT_MESSAGE, MESSAGES.length - 1)],
+        message: MESSAGES[getRandomPositiveInteger(MIN_COUNT_MESSAGES, MESSAGES.length - 1)],
         name: NAMES[getRandomPositiveInteger(MIN_COUNT_NAMES, NAMES.length - 1)],
       }
     );
@@ -127,30 +106,18 @@ const getComments = () => {
   return comments;
 };
 
-const getPhotos = () => {
-
+const createPhoto = () => {
   const photos = [];
-
-  for (let i = 1; i <= MAX_COUNT; i++) {
-    photos.push(
-      {
-        id: i,
-        url: `photos/${i}.jpg`,
-        description: DESCRIPTIONS[getRandomPositiveInteger(MIN_COUNT_DESCRIPTION, DESCRIPTIONS.length - 1)],
-        likes: getRandomPositiveInteger(MIN_LIKES, MAX_LIKES),
-        comments: getComments(),
-      }
-    );
+  for (let i = 1; i <= MAX_COUNT; i ++) {
+    photos.push({
+      id: getRandomPhotoId(),
+      url: `photos/${getRandomUrlId()}.jpg`,
+      likes: getRandomNumbersOfLikes(),
+      description: DESCRIPTIONS[getRandomDescription()],
+      comments: getComments(),
+    });
   }
-
   return photos;
 };
 
-getPhotos();
-
-const getRandomPhotoId = getRandomArrayElement(MIN_COUNT, MAX_COUNT);
-const getRandomUrlId = getRandomArrayElement(MIN_COUNT, MAX_COUNT);
-const getRandomNumbersOfLikes = getRandomArrayElement(MIN_LIKES, MAX_LIKES);
-const getRandomNumbersOfComments = getRandomArrayElement(MIN_COUNT_MESSAGE, MAX_COUNT_MESSAGE);
-const getRandomAvatarId = getRandomArrayElement(MIN_COUNT_AVATAR, MAX_COUNT_AVATAR);
-const getRandomNumber = getRandomArrayElement(MIN_COUNT , MAX_LIKES);
+createPhoto();
