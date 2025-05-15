1615b70ee7679d94bd9512a774ec5cc33a892276
MaksimDoronin <maksdoroninone@yandex.ru>
2025-05-15 12:10:19 +0300

    Выполнил домашнее задание 5.16. Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -49,3 +49,50 @@ getIntPosNumber('а я томат');
 getIntPosNumber(2023);
 getIntPosNumber(-1);
 getIntPosNumber(1.5);
+
+/*
+ 5.16. Функции возвращаются
+ Задача: Делу — время
+
+ Напишите функцию, которая принимает время начала и конца рабочего дня,
+ а также время старта и продолжительность встречи в минутах и возвращает
+ true, если встреча не выходит за рамки рабочего дня, и false, если выходит.
+
+ Время указывается в виде строки в формате часы:минуты. Для указания часов
+ и минут могут использоваться как две цифры, так и одна.
+ Например, 8 часов 5 минут могут быть указаны по-разному: 08:05, 8:5, 08:5 или 8:05.
+
+ Продолжительность задаётся числом. Гарантируется, что и рабочий день,
+ и встреча укладываются в одни календарные сутки.
+*/
+
+const convertStringToNumber = (str) => Math.abs(str.split(':')[0]) * 60 + Math.abs(str.split(':')[1]);
+
+
+const isMeetingBeyondWorkingDay = (value1, value2, value3, value4) => {
+  const startDay = convertStringToNumber(value1);
+  const endDay = convertStringToNumber(value2);
+  const startMeeting = convertStringToNumber(value3);
+  const durationMeeting = value4;
+
+  // console.log(`Рабочий день начинается с ${startDay} минуты`);
+  // console.log(`Рабочий день заканчивается на ${endDay} минуте`);
+  // console.log(`Встреча начинается на ${startMeeting} минуте`);
+  // console.log(`Длительность встречи составляет ${durationMeeting} минут`);
+
+  if(startMeeting < startDay) {
+    return false;
+  }
+
+  return (startMeeting + durationMeeting) <= endDay;
+
+};
+
+isMeetingBeyondWorkingDay('8:00', '17:30', '08:00', 900);
+
+//Проверка
+// имяФункции('08:00', '17:30', '14:00', 90); // true
+// имяФункции('8:0', '10:0', '8:0', 120);     // true
+// имяФункции('08:00', '14:30', '14:00', 90); // false
+// имяФункции('14:00', '17:30', '08:0', 90);  // false
+// имяФункции('8:00', '17:30', '08:00', 900); // false
