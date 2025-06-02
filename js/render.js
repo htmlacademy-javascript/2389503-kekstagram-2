538fd313729f88585ebe29f401dc4c5062b5eab4
MaksimDoronin <maksdoroninone@yandex.ru>
2025-06-02 13:15:39 +0300

    Заполняю данными шаблон карточки изображения пользователя

--- js/render.js
+++ js/render.js
@@ -5,12 +5,19 @@ const renderCards = (photos) => {
   const photoTemplate = document.querySelector('#picture')
     .content
     .querySelector('.picture');
-  // console.log(photoTemplate);
+  console.log(photoTemplate);
   // Создаю коробочку, в которую я буду складывать отображённые фотографии
   const similarListFragment = document.createDocumentFragment();
 
   photos.forEach((photo) => {
-    // console.log(photo);
+    console.log(photo.likes);
+    const cloneItem = photoTemplate.cloneNode(true);
+    const pictureImage = cloneItem.querySelector('.picture__img');
+    const pictureLikes = cloneItem.querySelector('.picture__likes');
+    pictureImage.src = photo.url;
+    pictureImage.alt = photo.description;
+    pictureLikes.textContent = photo.likes;
+    console.log(cloneItem);
   });
 };
 
