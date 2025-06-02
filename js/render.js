b23736ee08f5f9e2c2aee73526a33de23fc55b48
MaksimDoronin <maksdoroninone@yandex.ru>
2025-06-02 12:14:03 +0300

    Нахожу DOM-элементы и связываю их с объектами JS

--- js/render.js
+++ js/render.js
@@ -1,3 +1,17 @@
-export const renderCards = (photos) => {
-  photos.forEach(() => {});
+const renderCards = (photos) => {
+  // Нахожу контейнер для изображений от других пользователей
+  const picturesContainer = document.querySelector('.pictures');
+  // Нахожу шаблон фотографии
+  const photoTemplate = document.querySelector('#picture')
+    .content
+    .querySelector('.picture');
+  // console.log(photoTemplate);
+  // Создаю коробочку, в которую я буду складывать отображённые фотографии
+  const similarListFragment = document.createDocumentFragment();
+
+  photos.forEach((photo) => {
+    // console.log(photo);
+  });
 };
+
+export { renderCards };
