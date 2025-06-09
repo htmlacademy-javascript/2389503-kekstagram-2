266d7404602102d396645c1a2e35b6e79abd885c
MaksimDoronin <maksdoroninone@yandex.ru>
2025-06-09 11:22:56 +0300

    Тренируюсь использовать способ делегирование

--- js/render.js
+++ js/render.js
@@ -8,15 +8,15 @@ const renderCards = (photos) => {
   // Создаю коробочку, в которую я буду складывать отображённые фотографии
   const similarListFragment = document.createDocumentFragment();
 
-  photos.forEach((photo) => {
+  photos.forEach(({ url, description, likes, comments }) => {
     const cloneItem = photoTemplate.cloneNode(true);
     const pictureImage = cloneItem.querySelector('.picture__img');
     const pictureLikes = cloneItem.querySelector('.picture__likes');
     const pictureComments = cloneItem.querySelector('.picture__comments');
-    pictureImage.src = photo.url;
-    pictureImage.alt = photo.description;
-    pictureLikes.textContent = photo.likes;
-    pictureComments.textContent = photo.comments.length;
+    pictureImage.src = url;
+    pictureImage.alt = description;
+    pictureLikes.textContent = likes;
+    pictureComments.textContent = comments.length;
     similarListFragment.appendChild(cloneItem);
   });
 
