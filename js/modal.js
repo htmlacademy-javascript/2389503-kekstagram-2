bb1d3c128fd77b6b4cb8111d3328f16a18d3458b
MaksimDoronin <maksdoroninone@yandex.ru>
2025-06-16 13:24:47 +0300

    Заполняю список комментариев данными

--- js/modal.js
+++ js/modal.js
@@ -3,11 +3,14 @@ import { isEscapeKey } from './util.js';
 const modal = document.querySelector('.big-picture');
 const closeButton = modal.querySelector('.big-picture__cancel');
 const photoPreview = modal.querySelector('.big-picture__img').querySelector('img');
+const caption = modal.querySelector('.social__caption');
 const likesCount = modal.querySelector('.likes-count');
+const commentCount = modal.querySelector('.social__comment-count');
 const commentShownCount = modal.querySelector('.social__comment-shown-count');
 const commentTotalCount = modal.querySelector('.social__comment-total-count');
 const listOfComments = modal.querySelector('.social__comments');
 const comment = modal.querySelector('.social__comment');
+const commentsLoader = modal.querySelector('.social__comments-loader');
 const onDocumentKeydown = (evt) => {
   if(isEscapeKey(evt)) {
     modal.classList.add('hidden');
@@ -15,12 +18,16 @@ const onDocumentKeydown = (evt) => {
 };
 
 export const openModal = (id, url, description, likes, comments) => {
+  document.body.classList.add('modal-open');
   modal.classList.remove('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
   photoPreview.src = url;
+  caption.textContent = description;
+  commentCount.classList.add('hidden');
   likesCount.textContent = likes;
   commentShownCount.textContent = listOfComments.children.length;
   commentTotalCount.textContent = comments.length;
+  commentsLoader.classList.add('hidden');
   for (let i = 0; i < comments.length; i++) {
     const cloneComment = comment.cloneNode(true);
     const cloneCommentAvatar = cloneComment.querySelector('.social__picture');
@@ -38,6 +45,7 @@ const closeModal = () => {
   // 2. Удалить обработчики для закрытия (на всяком документе)
   document.removeEventListener('keydown', onDocumentKeydown);
   // 3. Прочая логика
+  document.body.classList.remove('modal-open');
 };
 
 // Подключаю обработчик клика на кнопку закрытия
