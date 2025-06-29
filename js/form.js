5b2da639bc89470093b978acb81df144b724dccd
MaksimDoronin <maksdoroninone@yandex.ru>
2025-06-29 22:57:02 +0300

    Реализую открытие модального окна для редакт изобр

--- js/form.js
+++ js/form.js
@@ -1,10 +1,17 @@
 // Данный модуль отвечает за работу с формой
+//
 // Нахожу форму
 const uploadForm = document.querySelector('.img-upload__form');
 // Нахожу поле для загрузки файла
 const imgUploadField = uploadForm.querySelector('.img-upload__input');
-console.log(imgUploadField);
+// Нахожу элемент формы для редактирования изображения - модальное окно
+const imgUploadModal = uploadForm.querySelector('.img-upload__overlay');
+// Функция-обработчик
+const onImgUploadFieldChange = () => {
+  // Открываю модальное окно
+  imgUploadModal.classList.remove('hidden');
+  // Выключаю прокрутку в фоне
+  document.body.classList.add('modal-open');
+};
 // Подключаю к полю для загрузки файла слушатель события "change"
-imgUploadField.addEventListener('click', () => {
-  console.log('Click!');
-});
+imgUploadField.addEventListener('change', onImgUploadFieldChange);
