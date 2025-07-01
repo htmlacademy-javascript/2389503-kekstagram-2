c772b65665f72a126480592c9ea1faa097a3f430
MaksimDoronin <maksdoroninone@yandex.ru>
2025-07-01 20:08:10 +0300

    Добавляю валидатор для проверки валидности хэштега

--- js/form.js
+++ js/form.js
@@ -3,6 +3,7 @@ const uploadForm = document.querySelector('.img-upload__form');
 const uploadField = uploadForm.querySelector('.img-upload__input');
 const modal = uploadForm.querySelector('.img-upload__overlay');
 const closeButton = uploadForm.querySelector('.img-upload__cancel');
+const hashtagsField = uploadForm.querySelector('.text__hashtags');
 // const zoomOutButton = uploadForm.querySelector('.scale__control--smaller');
 // const zoomInButton = uploadForm.querySelector('.scale__control--bigger');
 // const scaleControlInput = uploadForm.querySelector('.scale__control--value');
@@ -45,7 +46,20 @@ const pristine = new Pristine(uploadForm, {
   errorTextClass: 'img-upload__field-wrapper--error',
 });
 
+const validateHashtagsField = (value) => {
+  // введён невалидный хэштег
+  const regexp = /^#[a-zа-яё0-9]{1,19}$/i;
+  // Разбиваю строку на хэштеги с помощью шаблона " " - пробела и помещаю из в массив
+  const hashtags = value.split(' ');
+  // Итерируюсь по полученному массиву с хэштэгами и проверяю соответствуют ли все хэштеги regexp - ругулярному выражению
+  // Если хотя бы один хэштег не соответствует regexp, то поле не проходит валидацию
+  return hashtags.every((item) => regexp.test(item));
+};
+
+pristine.addValidator(hashtagsField, validateHashtagsField, 'Введён невалидный хэштег');
+
 uploadForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   pristine.validate();
 });
+
