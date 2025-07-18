c7e39577444db0687b2a3bed1ccbc9cc398c580f
MaksimDoronin <maksdoroninone@yandex.ru>
2025-07-18 11:59:37 +0300

    Устанавливаю определение успешности выполнения запроса response.ok при получении данных

--- js/api.js
+++ js/api.js
@@ -1,7 +1,14 @@
 import { ServerAddresses } from './constants.js';
 
 export const getData = () => fetch(ServerAddresses.GETTING)
-  .then((response) => response.json());
+  .then((response) => {
+    if(response.ok) {
+      return response.json();
+    }
+
+    const { status, statusText } = response;
+    throw new Error(`${status} - ${statusText}`);
+  });
 
 export const sendData = (body) => fetch(
   ServerAddresses.SENDING,
--- js/main.js
+++ js/main.js
@@ -4,4 +4,5 @@ import { renderCards } from './render.js';
 import { getData } from './api.js';
 
 getData()
-  .then((cards) => renderCards(cards));
+  .then((cards) => renderCards(cards))
+  .catch((err) => console.log(err));
